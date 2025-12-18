d61654995b76c6321bf5988cc32516e224b640f0
EvgenyEVS <evis10@yandex.ru>
2025-12-18 22:04:55 +0300

    Initial Commit

---
+++ src/test/java/org/skypro/homeworks/hogwarts1/Hogwarts1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.homeworks.hogwarts1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogwarts1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
