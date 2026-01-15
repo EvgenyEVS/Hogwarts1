2def27728d444ddf1ce46aca4f3affcd7b803271
EvgenyEVS <evis10@yandex.ru>
2026-01-15 22:29:32 +0300

    7

--- src/test/java/org/skypro/homeworks/hogwarts1/StudentControllerTestRestTemplate.java
+++ src/test/java/org/skypro/homeworks/hogwarts1/StudentControllerTestRestTemplate.java
@@ -15,12 +15,15 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import javax.transaction.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.LONG;
@@ -141,7 +144,7 @@ public class StudentControllerTestRestTemplate {
 
 
     @Test
-    public void removeStudentTest () throws Exception {
+    public void removeStudentTest() throws Exception {
 
         String createUrl = "http://localhost:" + port + "/student";
         StudentCreateDto studentCreateDto = new StudentCreateDto("Старый Создаванов", 999);
@@ -162,10 +165,156 @@ public class StudentControllerTestRestTemplate {
                 removeUrl,
                 StudentResponseDto.class
         );
-       assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-       assertThat(getResponse.getBody()).isNull();
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(getResponse.getBody()).isNull();
     }
 
+
+    @Test
+    public void findStudentByNameOrColorOrAllTestWhenName() throws Exception {
+
+        String createUrl = "http://localhost:" + port + "/student";
+        StudentCreateDto studentCreateDto1 = new StudentCreateDto("Тестируемый поиск1", 111);
+        StudentCreateDto studentCreateDto2 = new StudentCreateDto("Тестируемый поиск2", 222);
+        StudentCreateDto studentCreateDto3 = new StudentCreateDto("Неподходящее НеИмя", 333);
+
+        ResponseEntity<Student> createResponse1 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto1,
+                Student.class
+        );
+        ResponseEntity<Student> createResponse2 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto2,
+                Student.class
+        );
+        ResponseEntity<Student> createResponse3 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto3,
+                Student.class
+        );
+
+        String searchUrl = "http://localhost:" + port + "/student/search?name=Тестируемый поиск";
+
+        ResponseEntity<List<StudentResponseDto>> responseSearch = restTemplate.exchange(
+                searchUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<StudentResponseDto>>() {
+                }
+        );
+
+        assertThat(responseSearch.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseSearch.getBody()).isNotNull();
+
+        List<StudentResponseDto> foundStudents = responseSearch.getBody();
+
+        for (StudentResponseDto student : foundStudents) {
+            assertThat(student.name()).contains("Тестируемый поиск");
+            assertThat(student.name()).doesNotContain("Неподходящее НеИмя");
+        }
+
+        for (StudentResponseDto student : foundStudents) {
+            assertThat(student.age()).isIn(111, 222);
+            assertThat(student.age()).isNotEqualTo(333);
+        }
+
+    }
+
+
+
+    @Test
+    public void findStudentByNameOrColorOrAllTestWhenIsNotNameNotAge () throws Exception {
+
+        String createUrl = "http://localhost:" + port + "/student";
+        StudentCreateDto studentCreateDto1 = new StudentCreateDto("Тестируемый поиск1", 111);
+        StudentCreateDto studentCreateDto2 = new StudentCreateDto("Тестируемый поиск2", 222);
+        StudentCreateDto studentCreateDto3 = new StudentCreateDto("Неподходящее НеИмя", 333);
+
+        ResponseEntity<Student> createResponse1 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto1,
+                Student.class
+        );
+        ResponseEntity<Student> createResponse2 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto2,
+                Student.class
+        );
+        ResponseEntity<Student> createResponse3 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto3,
+                Student.class
+        );
+
+        String searchUrl = "http://localhost:" + port + "/student/search";
+
+        ResponseEntity<List<StudentResponseDto>> responseSearch = restTemplate.exchange(
+                searchUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<StudentResponseDto>>() {
+                }
+        );
+
+        List<StudentResponseDto> foundStudents = responseSearch.getBody();
+
+       boolean hasAdded1 = foundStudents.stream()
+               .anyMatch(s -> s.name().contains("Тестируемый поиск"));
+        boolean hasAdded2 = foundStudents.stream()
+                .anyMatch(s -> s.name().contains("Неподходящее НеИмя"));
+       assertThat(hasAdded1).isTrue();
+       assertThat(hasAdded2).isTrue();
+    }
+
+
+    @Test
+    public void findByAgeBetween() throws Exception {
+
+        String createUrl = "http://localhost:" + port + "/student";
+        StudentCreateDto studentCreateDto1 = new StudentCreateDto("Тестируемый поиск1", 111);
+        StudentCreateDto studentCreateDto2 = new StudentCreateDto("Тестируемый поиск2", 222);
+        StudentCreateDto studentCreateDto3 = new StudentCreateDto("Неподходящее НеИмя", 333);
+
+        ResponseEntity<Student> createResponse1 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto1,
+                Student.class
+        );
+        ResponseEntity<Student> createResponse2 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto2,
+                Student.class
+        );
+        ResponseEntity<Student> createResponse3 = restTemplate.postForEntity(
+                createUrl,
+                studentCreateDto3,
+                Student.class
+        );
+
+        String searchUrl = "http://localhost:" + port + "/student/search/age/between?ageMin=110&ageMax=223";
+
+        ResponseEntity<List<StudentResponseDto>> responseSearch = restTemplate.exchange(
+                searchUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<StudentResponseDto>>() {
+                }
+        );
+
+        List<StudentResponseDto> foundStudents = responseSearch.getBody();
+
+        boolean hasAdded1 = foundStudents.stream()
+                .anyMatch(s -> s.age() == 111);
+        boolean hasAdded2 = foundStudents.stream()
+                .anyMatch(s -> s.age() == 222);
+        boolean hasAdded3 = foundStudents.stream()
+                .anyMatch(s -> s.age() != 333);
+        assertThat(hasAdded1).isTrue();
+        assertThat(hasAdded2).isTrue();
+        assertThat(hasAdded3).isTrue();
+
+    }
 
 }
 
