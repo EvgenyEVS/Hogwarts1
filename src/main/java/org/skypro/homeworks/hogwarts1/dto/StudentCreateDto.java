b16214762ebe97917d2cfeec385aa7e7af2ce324
EvgenyEVS <evis10@yandex.ru>
2026-01-27 22:40:27 +0300

    16

--- src/main/java/org/skypro/homeworks/hogwarts1/dto/StudentCreateDto.java
+++ src/main/java/org/skypro/homeworks/hogwarts1/dto/StudentCreateDto.java
@@ -8,8 +8,8 @@ public record StudentCreateDto(
         @Size(min = 2, max = 30, message = "Name должно быть от 2 до 30 символов")
         String name,
 
-        @NotBlank(message = "Хоть какой-то возраст укажите!")
-        @Size(min = 0, max = Integer.MAX_VALUE, message = "В Хогвартс не принимают " +
-                "с возрастом меньше 0 или больше Integer.MAX_VALUE")
+       // @NotBlank(message = "Хоть какой-то возраст укажите!")
+       // @Size(min = 0, max = Integer.MAX_VALUE, message = "В Хогвартс не принимают " +
+        //        "с возрастом меньше 0 или больше Integer.MAX_VALUE")
         int age) {
 }
